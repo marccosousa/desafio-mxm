e3844379db2a721b2778520e5e0a58ba95761acd
Marco Sousa <marcosousacontato@gmail.com>
2023-06-07 11:15:20 -0300

    Projeto criado

---
+++ backend/IAFerias.API/IAFerias.API/Controllers/FeriasController.cs
@@ -0,0 +1,20 @@
+﻿using IAFerias.API.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IAFerias.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeriasController : ControllerBase
+    {      
+ 
+    }
+}
